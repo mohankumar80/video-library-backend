c0581369e815f7326a11bdf70ca91ff0192e4cb5
mohankumar80 <purammohankumar@gmail.com>
2021-08-23 22:29:14 +0530

    fix: cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 const mongoose = require('mongoose');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const initializeDBConnection = require('./db/db.connect');
